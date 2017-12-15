30039d0708fa2b1faa5b16f6382186cb01969b34
Mariel Segovia <msegovia94@gmail.com>
2017-12-15 02:21:30 +0000

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,11 @@
 class SessionsController < ApplicationController
-  def new
-  end
-
   def create
-    return head(:unprocessable) unless params[:name]
     session[:user_id] = User.find_or_create_by(name: params[:name]).id
-    redirect_to '/'
+    redirect_to :back
   end
 
   def destroy
     session.delete :user_id
-    redirect_to '/'
+    redirect_to :back
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,6 +7,7 @@ class Ability
     end
 
     can :manage, Note, user_id: user.id
+
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
